3a68ad1f5beb2a6ad91b389acf1ce94b2942d6d4
Vid-T4D <65925089+Vid-T4D@users.noreply.github.com>
2023-12-01 19:15:29 +0000

    Add files via upload

---
+++ milestone_4.py
@@ -0,0 +1,57 @@
+#This is the first task in Hangman game, where a list is created and input is taken 
+import random
+
+word_list = ['banana', 'kiwi', 'mango', 'berry', 'pears'] #create a list for the game
+print(word_list)
+word = random.choice(word_list) # select a random word from the list
+print(word)
+
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+
+
+        self.word_list = [] # Initialize an empty list
+        self.num_lives = num_lives
+        self.num_letters = len(set(word))
+        self.list_of_guesses = []
+        self.word = word
+
+        self.word_guessed = ['_' for item in word]
+
+
+
+
+ #This method is to check whether the letter guessed by the user is in the word that was randomly chosen by the computer
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in word:
+            print(f"Good guess!'{guess}' is in the word.")
+            for index, letter in enumerate(word):
+                if letter == guess: 
+                    self.word_guessed[index] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry,'{guess}' is not in word.")
+            print(f"You have '{self.num_lives}' lives left")
+
+# This method is to  continuously ask the user for a letter and validate it.
+    def ask_for_input(self):
+        while(True):
+            guess = input("Enter a single character: ") # Ask the user for a input
+            if ((len(guess) != 1 ) and (guess.isalpha()== False)):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            
+            elif any(item in guess for item in self.list_of_guesses): # Checks if the guess is already in the list_of_guesses
+                print("You already tried that letter")
+            else: 
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                break
+                
+        
+
+my_instance = Hangman(word_list)
+my_instance.ask_for_input()
